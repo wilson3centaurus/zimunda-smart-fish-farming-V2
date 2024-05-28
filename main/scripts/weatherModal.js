26594a4988e2479f8b38e9af5bdf4cf4bc67c104
wilson3centaurus <tafadzwawilsonsedze@gmail.com>
2024-05-27 22:31:39 -0700

    update

--- main/scripts/weatherModal.js
+++ main/scripts/weatherModal.js
@@ -1,106 +1,108 @@
-const wrapper = document.querySelector(".wrapper"),
-  inputPart = document.querySelector(".input-part"),
-  infoTxt = inputPart.querySelector(".info-txt"),
-  inputField = inputPart.querySelector("input"),
-  locationBtn = inputPart.querySelector("button"),
-  weatherPart = wrapper.querySelector(".weather-part"),
-  wIcon = weatherPart.querySelector("img"),
-  arrowBack = wrapper.querySelector("header i");
+const cityInput = document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-btn");
+const locationButton = document.querySelector(".location-btn");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const weatherCardsDiv = document.querySelector(".weather-cards");
 
-let api;
+const API_KEY = "d8e17c4bf2e9c6c4a1e4dfaf970742b6";
+const DEFAULT_CITY = "Mutare";
 
-inputField.addEventListener("keyup", (e) => {
-  // if user pressed enter btn and input value is not empty
-  if (e.key == "Enter" && inputField.value != "") {
-    requestApi(inputField.value);
-  }
-});
-
-locationBtn.addEventListener("click", () => {
-  if (navigator.geolocation) {
-    // if browser support geolocation api
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+const createWeatherCard = (cityName, weatherItem, index) => {
+  if (index === 0) {
+    // HTML for the main weather card
+    return `<div class="detaiLls">
+                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                    <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(
+                      2
+                    )}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                </div>
+                <div class="icon">
+                    <img src="https://openweathermap.org/img/wn/${
+                      weatherItem.weather[0].icon
+                    }@4x.png" alt="weather-icon">
+                    <h6>${weatherItem.weather[0].description}</h6>
+                </div>`;
   } else {
-    alert("Your browser not support geolocation api");
+    // HTML for the other five day forecast card
+    return `<li class="card">
+                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                    <img src="https://openweathermap.org/img/wn/${
+                      weatherItem.weather[0].icon
+                    }@4x.png" alt="weather-icon">
+                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(
+                      2
+                    )}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                </li>`;
   }
-});
+};
 
-function requestApi(city) {
-  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=d8e17c4bf2e9c6c4a1e4dfaf970742b6`;
-  fetchData();
-}
+const getWeatherDetails = (cityName, latitude, longitude) => {
+  const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
 
-function onSuccess(position) {
-  const { latitude, longitude } = position.coords; // getting lat and lon of the user device from coords obj
-  api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=your_api_key`;
-  fetchData();
-}
+  fetch(WEATHER_API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      // Filter the forecasts to get only one forecast per day
+      const uniqueForecastDays = [];
+      const fiveDaysForecast = data.list.filter((forecast) => {
+        const forecastDate = new Date(forecast.dt_txt).getDate();
+        if (!uniqueForecastDays.includes(forecastDate)) {
+          return uniqueForecastDays.push(forecastDate);
+        }
+      });
 
-function onError(error) {
-  // if any error occur while getting user location then we'll show it in infoText
-  infoTxt.innerText = error.message;
-  infoTxt.classList.add("error");
-}
+      // Clearing previous weather data
+      cityInput.value = "";
+      currentWeatherDiv.innerHTML = "";
+      weatherCardsDiv.innerHTML = "";
 
-function fetchData() {
-  infoTxt.innerText = "Getting weather details...";
-  infoTxt.classList.add("pending");
-  // getting api response and returning it with parsing into js obj and in another
-  // then function calling weatherDetails function with passing api result as an argument
-  fetch(api)
-    .then((res) => res.json())
-    .then((result) => weatherDetails(result))
+      // Creating weather cards and adding them to the DOM
+      fiveDaysForecast.forEach((weatherItem, index) => {
+        const html = createWeatherCard(cityName, weatherItem, index);
+        if (index === 0) {
+          currentWeatherDiv.insertAdjacentHTML("beforeend", html);
+        } else {
+          weatherCardsDiv.insertAdjacentHTML("beforeend", html);
+        }
+      });
+    })
     .catch(() => {
-      infoTxt.innerText = "Something went wrong";
-      infoTxt.classList.replace("pending", "error");
+      showError();
     });
-}
+};
 
-function weatherDetails(info) {
-  if (info.cod == "404") {
-    // if user entered city name isn't valid
-    infoTxt.classList.replace("pending", "error");
-    infoTxt.innerText = `${inputField.value} isn't a valid city name`;
-  } else {
-    //getting required properties value from the whole weather information
-    const city = info.name;
-    const country = info.sys.country;
-    const { description, id } = info.weather[0];
-    const { temp, feels_like, humidity } = info.main;
+const getCityCoordinates = (cityName) => {
+  const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
 
-    // using custom weather icon according to the id which api gives to us
-    if (id == 800) {
-      wIcon.src = "icons/clear.svg";
-    } else if (id >= 200 && id <= 232) {
-      wIcon.src = "icons/storm.svg";
-    } else if (id >= 600 && id <= 622) {
-      wIcon.src = "icons/snow.svg";
-    } else if (id >= 701 && id <= 781) {
-      wIcon.src = "icons/haze.svg";
-    } else if (id >= 801 && id <= 804) {
-      wIcon.src = "icons/cloud.svg";
-    } else if ((id >= 500 && id <= 531) || (id >= 300 && id <= 321)) {
-      wIcon.src = "icons/rain.svg";
-    }
+  // Get city coordinates (latitude, longitude, and name) from the API response
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      if (!data.length) return alert(`No coordinates found for ${cityName}`);
+      const { lat, lon, name } = data[0];
+      getWeatherDetails(name, lat, lon);
+    })
+    .catch(() => {
+      alert("An error occurred while fetching the coordinates!");
+    });
+};
 
-    //passing a particular weather info to a particular element
-    weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
-    weatherPart.querySelector(".weather").innerText = description;
-    weatherPart.querySelector(
-      ".location span"
-    ).innerText = `${city}, ${country}`;
-    weatherPart.querySelector(".temp .numb-2").innerText =
-      Math.floor(feels_like);
-    weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
-    infoTxt.classList.remove("pending", "error");
-    infoTxt.innerText = "";
-    inputField.value = "";
-    wrapper.classList.add("active");
-  }
-}
 
-arrowBack.addEventListener("click", () => {
-  wrapper.classList.remove("active");
+document.addEventListener("DOMContentLoaded", () => {
+  getCityCoordinates(DEFAULT_CITY);
 });
 
 
+
+function closeModal() {
+  const weatherModal = document.querySelector(".weather-modal");
+  weatherModal.style.display = "none";
+}
+function openModal() {
+  const weatherModal = document.querySelector(".weather-modal");
+  weatherModal.style.display = "block";
+}
